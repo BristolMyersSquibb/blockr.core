1e125868ef1d4031e4c03091b562c18cd6d9f8a7
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-12-11 10:28:10 +0100

    simplify core code export

--- R/utils-code.R
+++ R/utils-code.R
@@ -28,11 +28,21 @@ export_code <- function(expressions, board) {
 
   ordering <- topo_sort(as.matrix(board))
 
-  exprs <- Map(
-    wrap_expr,
-    expressions[ordering],
-    arg_map[ordering],
-    expr_type[ordering]
+  list(
+    exprs = expressions[ordering],
+    args = set_names(arg_map[ordering], ordering),
+    types = expr_type[ordering]
+  )
+}
+
+export_wrapped_code <- function(expressions, board) {
+
+  exprs <- do.call(
+    map,
+    c(
+      list(wrap_expr),
+      export_code(expressions, board)
+    )
   )
 
   exprs <- map(assignment, names(exprs), exprs)
@@ -42,102 +52,19 @@ export_code <- function(expressions, board) {
   paste0(exprs, collapse = "\n\n")
 }
 
-wrap_expr <- function(expr, env, type) {
+wrap_expr <- function(exprs, args, types) {
 
-  if (identical(type, "bquoted")) {
-    expr <- do.call(bquote, list(expr, env))
+  if (identical(types, "bquoted")) {
+    exprs <- do.call(bquote, list(exprs, args))
   }
 
-  if (length(env) && identical(type, "quoted")) {
-    call("with", env, expr)
-  } else if (has_assignment(expr)) {
-    call("local", expr)
+  if (length(args) && identical(types, "quoted")) {
+    call("with", args, exprs)
   } else {
-    expr
+    call("local", exprs)
   }
 }
 
 assignment <- function(name, value) {
   bquote(.(nme) <- .(val), list(nme = as.name(name), val = value))
-}
-
-has_assignment <- function(expr) {
-  check_assignment_recursive(expr, local_scope = FALSE)
-}
-
-check_assignment_recursive <- function(expr, local_scope) {
-
-  if (is.atomic(expr) || is.name(expr)) {
-    return(FALSE)
-  }
-
-  if (is.call(expr)) {
-
-    fn <- as.character(expr[[1]])
-
-    if (fn == "<-" && !local_scope) {
-      return(TRUE)
-    }
-
-    if (fn %in% c("<<-", "assign")) {
-
-      blockr_warn(
-        "Using the global assignment operator `<<-` or calling ",
-        "assign() is discouraged, as it can lead to unreliable ",
-        "code generation.",
-        class = "code_generation_discouraged_assignments"
-      )
-
-      return(TRUE)
-    }
-
-    if (fn == "function" || fn == "\\") {
-
-      if (length(expr) >= 3) {
-        return(
-          check_assignment_recursive(expr[[3L]], local_scope = TRUE)
-        )
-      }
-
-      return(FALSE)
-    }
-
-    if (fn == "local") {
-
-      if (length(expr) >= 2) {
-        return(
-          check_assignment_recursive(expr[[2]], local_scope = TRUE)
-        )
-      }
-
-      return(FALSE)
-    }
-
-    if (fn == "{") {
-
-      for (i in seq_along(expr)[-1]) {
-        if (check_assignment_recursive(expr[[i]], local_scope)) {
-          return(TRUE)
-        }
-      }
-
-      return(FALSE)
-    }
-
-    for (i in seq_along(expr)[-1]) {
-      if (check_assignment_recursive(expr[[i]], local_scope)) {
-        return(TRUE)
-      }
-    }
-  }
-
-  if (is.pairlist(expr)) {
-    for (i in seq_along(expr)) {
-      if (check_assignment_recursive(expr[[i]], local_scope)) {
-        return(TRUE)
-      }
-    }
-  }
-
-  FALSE
 }
--- tests/testthat/test-utils-code.R
+++ tests/testthat/test-utils-code.R
@@ -1,319 +1,38 @@
-test_that("cove generation utils", {
+test_that("code generation", {
 
-  expect_false(has_assignment(quote(x + y)))
-  expect_true(has_assignment(quote(x <- 5)))
-  expect_false(has_assignment(quote(f(x, y))))
-  expect_false(has_assignment(quote(1 + 2)))
-  expect_false(has_assignment(quote(x)))
-
-  expect_warning(
-    expect_true(has_assignment(quote(x <<- 5))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-
-  expect_warning(
-    expect_true(has_assignment(quote(assign("x", 5)))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_true(
-    has_assignment(
-      quote(
-        {
-          x <- 1
-        }
+  board <- generate_plugin_args(
+    new_board(
+      blocks = c(
+        a = new_dataset_block("BOD"),
+        b = new_dataset_block("ChickWeight"),
+        c = new_merge_block(by = "Time")
+      ),
+      links = links(
+        from = c("a", "b"),
+        to = c("c", "c"),
+        input = c("x", "y")
       )
     )
+  )$board
+
+  code_exp <- export_code(
+    isolate(lst_xtr_reval(board$blocks, "server", "expr")),
+    isolate(board$board)
   )
 
-  expect_true(
-    has_assignment(
-      quote(
-        {
-          y <- 1
-          z <- 2
-        }
-      )
-    )
+  expect_type(code_exp, "list")
+  expect_length(code_exp, 3L)
+  expect_named(code_exp, c("exprs", "args", "types"))
+
+  expect_named(code_exp[["exprs"]], c("a", "b", "c"), ignore.order = TRUE)
+  expect_named(code_exp[["args"]], names(code_exp[["exprs"]]))
+  expect_named(code_exp[["types"]], names(code_exp[["exprs"]]))
+
+  code_str <- export_wrapped_code(
+    isolate(lst_xtr_reval(board$blocks, "server", "expr")),
+    isolate(board$board)
   )
 
-  expect_true(has_assignment(quote(x <- 5)))
-  expect_true(has_assignment(quote(y <- x + 1)))
-  expect_true(has_assignment(quote(result <- f(x))))
-
-  expect_true(has_assignment(quote(f(x <- 5))))
-  expect_true(has_assignment(quote(g(a, b <- 2))))
-  expect_true(has_assignment(quote(plot(x, y <- x^2))))
-
-  expect_false(
-    has_assignment(
-      quote(function() x <- 5)
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(
-        function(a) {
-          x <- a
-        }
-      )
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(
-        function(a, b) {
-          x <- a + b
-          y <- x * 2
-        }
-      )
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(
-        \() {
-          x <- 5
-        }
-      )
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(
-        \(a) {
-          x <- a
-        }
-      )
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(\(a, b) x <- a + b)
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(
-        local(
-          {
-            x <- 5
-          }
-        )
-      )
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(
-        local(
-          {
-            x <- 1
-            y <- 2
-          }
-        )
-      )
-    )
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(x <<- 5))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(f(x <<- 5)))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(
-      has_assignment(
-        quote(
-          function() {
-            x <<- 5
-          }
-        )
-      )
-    ),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(
-      has_assignment(
-        quote(
-          local(
-            {
-              x <<- 5
-            }
-          )
-        )
-      )
-    ),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(\() x <<- 5))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(assign("x", 5)))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(f(assign("x", 5))))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(function() assign("x", 5)))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(local(assign("x", 5))))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(has_assignment(quote(\() assign("x", 5)))),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_true(has_assignment(quote(f(g(x <- 5)))))
-
-  expect_false(
-    has_assignment(
-      quote(
-        lapply(
-          data,
-          function(x) {
-            y <- x * 2
-          }
-        )
-      )
-    )
-  )
-
-  expect_true(
-    has_assignment(quote(lapply(data <- getData(), function(x) x * 2)))
-  )
-
-  expect_warning(
-    expect_true(
-      has_assignment(
-        quote(
-          function() {
-            x <<- 5
-          }
-        )
-      )
-    ),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_warning(
-    expect_true(
-      has_assignment(
-        quote(
-          local(
-            {
-              x <<- 5
-              y <- 3
-            }
-          )
-        )
-      )
-    ),
-    class = "code_generation_discouraged_assignments"
-  )
-
-  expect_true(
-    has_assignment(
-      quote(
-        {
-          x <- 5
-          f <- function() {
-            y <- 10
-          }
-        }
-      )
-    )
-  )
-
-  expect_true(
-    has_assignment(
-      quote(
-        {
-          f <- function() {
-            y <- 10
-          }
-        }
-      )
-    )
-  )
-
-  expect_false(has_assignment(quote({})))
-  expect_false(has_assignment(quote(42)))
-  expect_false(has_assignment(quote("hello")))
-  expect_false(has_assignment(quote(NULL)))
-  expect_true(has_assignment(quote(x <- 5)))
-
-  expect_true(
-    has_assignment(
-      quote({
-        if (x <- 5) {
-          print(x)
-        }
-      })
-    )
-  )
-
-  expect_false(
-    has_assignment(
-      quote(
-        {
-          for (i in 1:10) {
-            print(i)
-          }
-        }
-      )
-    )
-  )
-
-  expect_true(
-    has_assignment(
-      quote(
-        {
-          for (i in 1:10) {
-            x <- i
-          }
-        }
-      )
-    )
-  )
-
-  expect_true(
-    has_assignment(
-      quote(
-        {
-          while (x <- next_value()) {
-            process(x)
-          }
-        }
-      )
-    )
-  )
+  expect_type(code_str, "character")
+  expect_length(code_str, 1L)
 })
