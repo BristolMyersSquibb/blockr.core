996348ff2b99f997daaff44ff6a136098fef612b
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2026-03-20 14:15:30 +0000

    Add unit tests for meta round-trip in board restore

--- tests/testthat/test-plugin-serdes.R
+++ tests/testthat/test-plugin-serdes.R
@@ -115,6 +115,95 @@ test_that("gen_code return validation", {
   )
 })
 
+test_that("restore_board with meta wraps result", {
+
+  test_board <- new_board(
+    blocks = c(a = new_dataset_block("iris"))
+  )
+
+  ser <- blockr_ser(test_board)
+  result <- reactiveVal()
+  meta <- list(url = "/foo", flag = TRUE)
+
+  with_mock_session(
+    {
+      restore_board(test_board, ser, result, meta = meta, session = session)
+
+      val <- result()
+      expect_true(is.list(val))
+      expect_true(is_board(val$board))
+      expect_identical(val$meta, meta)
+      expect_length(board_blocks(val$board), length(board_blocks(test_board)))
+    }
+  )
+})
+
+test_that("restore_board without meta returns naked board", {
+
+  test_board <- new_board(
+    blocks = c(a = new_dataset_block("iris"))
+  )
+
+  ser <- blockr_ser(test_board)
+  result <- reactiveVal()
+
+  with_mock_session(
+    {
+      restore_board(test_board, ser, result, session = session)
+
+      val <- result()
+      expect_true(is_board(val))
+      expect_length(board_blocks(val), length(board_blocks(test_board)))
+    }
+  )
+})
+
+test_that("meta round-trip through serve_obj", {
+
+  id <- "test_meta_roundtrip"
+  on.exit(if (is_reloading(id)) finalize_reload(id), add = TRUE)
+
+  board <- new_board()
+  meta <- list(url = "/bar", n = 42L)
+
+  update_serve_obj(id, board, meta = meta)
+  expect_true(is_reloading(id))
+
+  result <- finalize_reload(id)
+  expect_identical(result, meta)
+  expect_false(is_reloading(id))
+})
+
+test_that("finalize_reload returns NULL when not reloading", {
+  expect_null(finalize_reload("nonexistent_test_id"))
+})
+
+test_that("update_serve_obj without meta stores NULL meta", {
+
+  id <- "test_no_meta"
+  on.exit(if (is_reloading(id)) finalize_reload(id), add = TRUE)
+
+  update_serve_obj(id, new_board())
+
+  result <- finalize_reload(id)
+  expect_null(result)
+})
+
+test_that("check_ser_deser_val accepts list with board and meta", {
+
+  board <- new_board()
+
+  with_mock_session(
+    {
+      val <- reactiveVal(list(board = board, meta = list(url = "/test")))
+      res <- check_ser_deser_val(val)
+      sink_msg(session$flushReact())
+
+      expect_identical(res, val)
+    }
+  )
+})
+
 test_that("dummy ser/deser ui test", {
   expect_s3_class(preserve_board_ui("ser_deser", new_board()), "shiny.tag.list")
 })
